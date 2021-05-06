4a3b2119d4b2a3a52529fbc420a40c1415e2aa5e
Utkarsh <utkarshkalra.2001@gmail.com>
2021-05-07 04:15:51 +0530

    In progress added down nav bar for phone

--- script/script.js
+++ script/script.js
@@ -1,35 +1,33 @@
 import projects_data from "./projects.js";
 
-const tech=(arr)=>{
-    let r='';
-    arr.forEach((name)=>{
-        r+=`<span>${name}</span> `
-
-    })
-    return r;
-}
+const tech = (arr) => {
+  let r = "";
+  arr.forEach((name) => {
+    r += `<span>${name}</span> `;
+  });
+  return r;
+};
 
 const projects = () => {
-    const div1 = document.getElementById("test");
-    let q='';
+  const div1 = document.getElementById("test");
+  let q = "";
   projects_data.forEach((project) => {
-      const {id,name,link,image,techStack,desc}=project
+    const { id, name, link, image, techStack, desc } = project;
     q +=
       `<div class="project">` +
       `<div class="image">` +
-      `<img src="img/projectphoto.jpg" alt=""></div>` +
+      `<img src="img/projectphoto.jpg" alt="${name}" onclick="window.open('${link}', '_blank');"></div>` +
       `<div class="details">` +
-      `<h3>${name}</h3>` +
-      `<a href="${link}" target="_blank">Link</a>` +
-      `<p>${desc}</p>`+
-      `</div>`+
+      `<a href="${link}" target="_blank"><h2>${name}</h2></a>` +
+      `<p>${desc}</p>` +
+      `</div><br>` +
       `<div class="tech">
-      <h3>Tech Stack:</h3><br>
+      <h3>Tech Stack:</h3>
       ${tech(techStack)}
-      </div>`+
+      </div>` +
       `</div>`;
   });
-  div1.innerHTML=q;
+  div1.innerHTML = q;
 };
 
 projects();
