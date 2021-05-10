dfa1c7a37ca05e0c07499c67d6bca7886f1dd62b
Utkarsh <utkarshkalra.2001@gmail.com>
2021-05-10 21:00:39 +0530

    back to top button to home

--- script/script.js
+++ script/script.js
@@ -31,3 +31,25 @@ const projects = () => {
 };
 
 projects();
+
+
+//scrolltop
+
+var mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+mybutton.addEventListener('click',()=>{
+
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+})
